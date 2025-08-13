ba3a3ab6294c00a7eec2c0113a08a56446865985
john <jfmmokoko@gmail.com>
2025-08-13 13:56:18 +0200

    creation de note de frais

---
+++ app/notefrais/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import NotefraisFrom from "../component/NotefraisFrom";
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white text-center">
+      <div>
+        <NotefraisFrom />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
